2995ec0b712afa2db272bfd3a3e52c70f932b9da
Vourliotis <x.vourliotis@gmail.com>
2021-08-14 12:17:14 +0300

    Add settings and clean up main()

--- main.py
+++ main.py
@@ -1,140 +1,113 @@
 from rtree import *
 
+
 def main():
     r_seed = 1
-    np.random.seed(seed = r_seed)
+    np.random.seed(seed=r_seed)
 
     rtree = RTree()
 
-    #ENTRIES = RECTANGLES
-    #entries = rtree.create_random(18, r_seed)
+    # VISUALIZATION SETTINGS
+    # PLOT THE DATA IN A SEPERATE WINDOW
+    plotData = False
+    # PRINT THE DATA IN THE CONSOLE
+    printInConsole = False
 
-    #ENTRIES = POINTS FROM CSV FILE
+    # ENTRIES = POINTS FROM CSV FILE
     data_set = 'data'
     entries, time_elapsed_tree = rtree.import_csv(data_set)
-
-    #ENTIRES = POINTS
+    # ENTRIES = POINTS RANDOMLY CREATED WITH A SEED
     #entries, time_elapsed_tree = rtree.create_random_points(5, r_seed)
 
-    #PRINT TREE IN CONSOLE
-    print(repr(rtree))
+    # PRINT TREE IN CONSOLE
+    if printInConsole == True:
+        print(repr(rtree))
 
-    #SHOW ENTRIES - POINTS
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect = 'equal')
-    plt.xlim([rtree.root.mbr.lower_left.x - 10, rtree.root.mbr.upper_right.x + 10])
-    plt.ylim([rtree.root.mbr.lower_left.y - 10, rtree.root.mbr.upper_right.y + 10])
-    patches = []
-    for i in range(len(entries)):
-        ax.add_patch(plt.Circle((entries[i].mbr.lower_left.x, entries[i].mbr.lower_left.y), 0.5, color = 'blue'))
-    plt.show(block = False)
+    if plotData == True:
+        # PLOT ENTRIES
+        plot_points(rtree, entries, False)
+        # PLOT R-TREE
+        rtree.show_points()
 
-    #SHOW ENTRIES - RECTANGLES
-    #fig = plt.figure()
-    #ax = fig.add_subplot(111, aspect = 'equal')
-    #plt.xlim([rtree.root.mbr.lower_left.x - 10, rtree.root.mbr.upper_right.x + 10])
-    #plt.ylim([rtree.root.mbr.lower_left.y - 10, rtree.root.mbr.upper_right.y + 10])
-    #patches = []
-    #for i in range(len(entries)):
-    #    ax.add_patch(matplotlib.patches.Rectangle((entries[i].mbr.lower_left.x, entries[i].mbr.lower_left.y), entries[i].mbr.width, entries[i].mbr.height, fill=None, edgecolor = c[0]))
-    #plt.show()
-
-    #PRINT R-TREE IF ENTRIES ARE RECTANGLES
-    #rtree.show()
-
-    #PRINT R-TREE IF ENTRIES ARE POINTS
-    rtree.show_points()
-
-    #PRINT SKYLINE ENTRIES IN CONSOLE
+    # PRINT SKYLINE ENTRIES IN CONSOLE
     skyline, time_elapsed_skyline = rtree.bbs_skyline()
-    print("\nShowing Skyline Points:")
-    for num_skylines in range(len(skyline)):
-        print("")
-        print(repr(skyline[num_skylines].mbr))
+    if printInConsole == True:
+        print("\nShowing Skyline Points:")
+        for num_skylines in range(len(skyline)):
+            print("")
+            print(repr(skyline[num_skylines].mbr))
 
-    #SHOW SKYLINE WITHOUT THE TREE
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect = 'equal')
-    plt.xlim([rtree.root.mbr.lower_left.x - 10, rtree.root.mbr.upper_right.x + 10])
-    plt.ylim([rtree.root.mbr.lower_left.y - 10, rtree.root.mbr.upper_right.y + 10])
-    patches = []
-    for i in range(len(entries)):
-        ax.add_patch(plt.Circle((entries[i].mbr.lower_left.x, entries[i].mbr.lower_left.y), 0.5, color = 'blue'))
-    for i in range(len(skyline)):
-        ax.add_patch(plt.Circle((skyline[i].mbr.lower_left.x, skyline[i].mbr.lower_left.y), 0.5, color = 'coral'))
-    plt.show(block = False)
+    if plotData == True:
+        # SHOW SKYLINE WITHOUT THE TREE
+        plot_points(rtree, entries, False, skyline)
+        # PRINT SKYLINE IN R-TREE
+        rtree.show_skyline(skyline)
 
-    #PRINT SKYLINE IN R-TREE
-    rtree.show_skyline(skyline)
-
-    #RANGE QUERY IN R-TREE
+    # RANGE QUERY IN R-TREE
     range_rec = Rectangle(Point(40, 40), Point(60, 60))
     query_entries, time_elapsed_query_entries = rtree.range_query(range_rec)
 
-    #PRINT RANGE QUERY ENTRIES IN CONSOLE
-    print("\nShowing Range Query Points")
-    for num_entries in range(len(query_entries)):
-        print("")
-        print(repr(query_entries[num_entries]))
-        
-    #SHOW ENTRIES - POINTS
+    # PRINT RANGE QUERY ENTRIES IN CONSOLE
+    if printInConsole == True:
+        print("\nShowing Range Query Points")
+        for num_entries in range(len(query_entries)):
+            print("")
+            print(repr(query_entries[num_entries]))
+
+    if plotData == True:
+        # SHOW ENTRIES - POINTS
+        plot_points(rtree, entries, False)
+        # SHOW QUERY ENTRIES WITHOUT THE R-TREE
+        plot_points(rtree, query_entries, True, None, range_rec)
+
+    # RANGE QUERY SKYLINE ENTRIES IN CONSOLE
+    skyline, time_elapsed_skyline_query = rtree.bbs_skyline_range_query(
+        range_rec)
+    if printInConsole == True:
+        print("\nShowing Skyline Points given a range query:")
+        for num_skylines in range(len(skyline)):
+            print("")
+            print(repr(skyline[num_skylines].mbr))
+
+    if plotData == True:
+        # SHOW ENTRIES - POINTS
+        plot_points(rtree, entries, False)
+        # SHOW SKYLINE ENTRIES GIVEN A RANGE QUERY
+        plot_points(rtree, query_entries, False, skyline, range_rec)
+        # SHOW SKYLINE ENTRIES GIVEN A RANGE QUERY IN THE R-TREE
+        rtree.show_skyline(skyline)
+
+    print("\nTime elapsed creating the tree: {:0.3f}s".format(
+        time_elapsed_tree))
+    print("Time elapsed finding the Skyline points: {:0.3f}s".format(
+        time_elapsed_skyline))
+    print("Time elapsed finding the range query entries: {:0.3f}s".format(
+        time_elapsed_query_entries))
+    print("Time elapsed finding the Skyline entries given a range: {:0.3f}s".format(
+        time_elapsed_skyline_query))
+
+
+def plot_points(rtree: RTree, entries: [Entry], show_block, skyline: [Entry] = None, range_rec=None):
     fig = plt.figure()
-    ax = fig.add_subplot(111, aspect = 'equal')
-    plt.xlim([rtree.root.mbr.lower_left.x - 10, rtree.root.mbr.upper_right.x + 10])
-    plt.ylim([rtree.root.mbr.lower_left.y - 10, rtree.root.mbr.upper_right.y + 10])
+    ax = fig.add_subplot(111, aspect='equal')
+    plt.xlim([rtree.root.mbr.lower_left.x - 10,
+             rtree.root.mbr.upper_right.x + 10])
+    plt.ylim([rtree.root.mbr.lower_left.y - 10,
+             rtree.root.mbr.upper_right.y + 10])
     patches = []
+    if range_rec is not None:
+        ax.add_patch(matplotlib.patches.Rectangle((range_rec.lower_left.x, range_rec.lower_left.y),
+                     range_rec.width, range_rec.height, fill=None, edgecolor='coral'))
     for i in range(len(entries)):
-        ax.add_patch(plt.Circle((entries[i].mbr.lower_left.x, entries[i].mbr.lower_left.y), 0.5, color = 'blue'))
-    plt.show(block = False)
+        ax.add_patch(plt.Circle(
+            (entries[i].mbr.lower_left.x, entries[i].mbr.lower_left.y), 0.5, color='blue'))
+    if skyline is not None:
+        for i in range(len(skyline)):
+            ax.add_patch(plt.Circle(
+                (skyline[i].mbr.lower_left.x, skyline[i].mbr.lower_left.y), 0.5, color='coral'))
+    plt.show(block=show_block)
+    return True
 
-    #SHOW QUERY ENTRIES WITHOUT THE R-TREE
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect = 'equal')
-    plt.xlim([rtree.root.mbr.lower_left.x - 10, rtree.root.mbr.upper_right.x + 10])
-    plt.ylim([rtree.root.mbr.lower_left.y - 10, rtree.root.mbr.upper_right.y + 10])
-    patches = []
-    ax.add_patch(matplotlib.patches.Rectangle((range_rec.lower_left.x, range_rec.lower_left.y), range_rec.width, range_rec.height, fill = None, edgecolor = 'coral'))
-    for i in range(len(query_entries)):
-        ax.add_patch(plt.Circle((query_entries[i].mbr.lower_left.x, query_entries[i].mbr.lower_left.y), 0.5, color = 'blue'))
-    plt.show()
-
-    #RANGE QUERY SKYLINE ENTRIES IN CONSOLE
-    skyline, time_elapsed_skyline_query = rtree.bbs_skyline_range_query(range_rec)
-    print("\nShowing Skyline Points given a range query:")
-    for num_skylines in range(len(skyline)):
-        print("")
-        print(repr(skyline[num_skylines].mbr))
-
-    #SHOW ENTRIES - POINTS
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect = 'equal')
-    plt.xlim([rtree.root.mbr.lower_left.x - 10, rtree.root.mbr.upper_right.x + 10])
-    plt.ylim([rtree.root.mbr.lower_left.y - 10, rtree.root.mbr.upper_right.y + 10])
-    patches = []
-    for i in range(len(entries)):
-        ax.add_patch(plt.Circle((entries[i].mbr.lower_left.x, entries[i].mbr.lower_left.y), 0.5, color = 'blue'))
-    plt.show(block = False)
-
-    #SHOW SKYLINE ENTRIES GIVEN A RANGE QUERY
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect = 'equal')
-    plt.xlim([rtree.root.mbr.lower_left.x - 10, rtree.root.mbr.upper_right.x + 10])
-    plt.ylim([rtree.root.mbr.lower_left.y - 10, rtree.root.mbr.upper_right.y + 10])
-    patches = []
-    ax.add_patch(matplotlib.patches.Rectangle((range_rec.lower_left.x, range_rec.lower_left.y), range_rec.width, range_rec.height, fill = None, edgecolor = 'coral'))
-    for i in range(len(query_entries)):
-        ax.add_patch(plt.Circle((query_entries[i].mbr.lower_left.x, query_entries[i].mbr.lower_left.y), 0.5, color = 'blue'))
-    for i in range(len(skyline)):
-        ax.add_patch(plt.Circle((skyline[i].mbr.lower_left.x, skyline[i].mbr.lower_left.y), 0.5, color = 'coral'))
-    plt.show(block = False)
-
-    #SHOW SKYLINE ENTRIES GIVEN A RANGE QUERY IN THE R-TREE
-    rtree.show_skyline(skyline)
-
-    print("\nTime elapsed creating the tree: " + str(time_elapsed_tree) + "s")
-    print("Time elapsed finding the Skyline points: " + str(time_elapsed_skyline) + "s")
-    print("Time elapsed finding the range query entries: " + str(time_elapsed_query_entries) + "s")
-    print("Time elapsed finding the Skyline entries given a range: " + str(time_elapsed_skyline_query) + "s")
 
 if __name__ == '__main__':
     main()
--- minList.py
+++ minList.py
@@ -4,7 +4,7 @@ class minList:
         self.mindist_list: [int] = []
         self.skyline: [Node] = []
 
-    #INSERTS A NODE OR AN ENTRY ALONG WITH ITS MINDIST AND SORTS BOTH LISTS WITH KEY=MINDIST
+    # INSERTS A NODE OR AN ENTRY ALONG WITH ITS MINDIST AND SORTS BOTH LISTS WITH KEY=MINDIST
     def insert(self, node: 'Node' = None, entry: 'Entry' = None, point: 'Point' = None):
         if node is not None:
             self.toDo_list.extend([node])
@@ -16,17 +16,18 @@ class minList:
             self.mindist_list.extend([node.mbr.mindist(point)])
         list1 = self.toDo_list
         list2 = self.mindist_list
-        self.toDo_list, self.mindist_list = (list(s) for s in zip(*sorted(zip(list1, list2), key=lambda pair: pair[1])))
+        self.toDo_list, self.mindist_list = (list(s) for s in zip(
+            *sorted(zip(list1, list2), key=lambda pair: pair[1])))
 
-    #CHECKS TO SEE IF THE MINLIST OBJECT IS EMPTY
+    # CHECKS TO SEE IF THE MINLIST OBJECT IS EMPTY
     def isEmpty(self):
         if len(self.mindist_list) != 0:
             return False
         else:
             return True
 
-    #OPENS UP THE FIRST NODE UNTUL WE REACH AN ENTRY. AN ENTRY IS THEN COMPARED WITH THE REST OF THE SKYLINE ENTRIES.
-    #IF THE ENTRY IS DOMINATED BY NO OTHER ENTRY INSIDE THE SKYLINE LIST, IT IS ADDED ASWELL.
+    # OPENS UP THE FIRST NODE UNTIL WE REACH AN ENTRY. AN ENTRY IS THEN COMPARED WITH THE REST OF THE SKYLINE ENTRIES.
+    # IF THE ENTRY IS DOMINATED BY NO OTHER ENTRY INSIDE THE SKYLINE LIST, IT IS ADDED ASWELL.
     def process(self, rec: 'Rectangle' = None):
         isDominatedFlag = 0
         if(len(self.toDo_list) != 0):
@@ -41,7 +42,7 @@ class minList:
                     if isDominatedFlag == 0:
                         for num_children in range(len(tmp_node.children)):
                             self.insert(tmp_node.children[num_children])
-                elif len(tmp_node.entries) !=0:
+                elif len(tmp_node.entries) != 0:
                     for num_skylines in range(len(self.skyline)):
                         if tmp_node.mbr.is_dominated(self.skyline[num_skylines].mbr):
                             isDominatedFlag = 1
@@ -65,11 +66,13 @@ class minList:
                     else:
                         if rec.contains(tmp_node.mbr):
                             self.skyline.extend([tmp_node])
-        
+
         return self.skyline
-        
+
     def __repr__(self):
         ret = ""
         for num_nodes in range(len(self.toDo_list)):
-            ret += "\nMindist: " + str(self.mindist_list[num_nodes]) + "\n" + self.toDo_list[num_nodes].mbr.__repr__() + "\n"
+            ret += "\nMindist: " + \
+                str(self.mindist_list[num_nodes]) + "\n" + \
+                self.toDo_list[num_nodes].mbr.__repr__() + "\n"
         return ret
